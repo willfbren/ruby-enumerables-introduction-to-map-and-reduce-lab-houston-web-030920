9b6246bc81e73ed3aee46843b4486e46053c47e8
Will Brennan <willfbren@gmail.com>
2020-02-29 21:07:24 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -15,29 +15,29 @@ def map_to_no_change(source_array)
   counter = 0
   
   while counter < source_array.size do
-    final_array.push()
+    final_array.push(source_array[counter])
     counter += 1 
   end
   final_array
 end
 
-def map_to_no_change(source_array)
+def map_to_double(source_array)
   final_array = []
   counter = 0
   
   while counter < source_array.size do
-    final_array.push()
+    final_array.push(source_array[counter] * 2)
     counter += 1 
   end
   final_array
 end
 
-def map_to_no_change(source_array)
+def map_to_square(source_array)
   final_array = []
   counter = 0
   
   while counter < source_array.size do
-    final_array.push()
+    final_array.push(source_array[counter] ^^ 2)
     counter += 1 
   end
   final_array
