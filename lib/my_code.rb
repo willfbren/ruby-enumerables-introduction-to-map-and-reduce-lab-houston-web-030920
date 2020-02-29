ebb4594666e85d1769d7c13957cf65c9daec5c3e
Will Brennan <willfbren@gmail.com>
2020-02-29 21:04:46 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -4,7 +4,8 @@ def map_to_negativize(source_array)
   counter = 0
   
   while counter < source_array.size do
-    
+    final_array.push(source_array[counter] * -1)
     counter += 1 
   end
+  final_array
 end
