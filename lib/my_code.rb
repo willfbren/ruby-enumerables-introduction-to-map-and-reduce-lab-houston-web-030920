70b804f86b86976ae1a91c158ca681b991d3c23d
Will Brennan <willfbren@gmail.com>
2020-02-29 21:06:34 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -20,3 +20,25 @@ def map_to_no_change(source_array)
   end
   final_array
 end
+
+def map_to_no_change(source_array)
+  final_array = []
+  counter = 0
+  
+  while counter < source_array.size do
+    final_array.push()
+    counter += 1 
+  end
+  final_array
+end
+
+def map_to_no_change(source_array)
+  final_array = []
+  counter = 0
+  
+  while counter < source_array.size do
+    final_array.push()
+    counter += 1 
+  end
+  final_array
+end
