aa1d734be8bcde0d06a4e22058a4e1926299dbac
Will Brennan <willfbren@gmail.com>
2020-02-29 21:21:54 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -68,7 +68,7 @@ def reduce_to_any_true(source_array)
   counter = 0
   
   while counter < source_array.size do
-    return true if CONDITION
+    return true if source_array[counter]
     counter += 1 
   end
   return false 
